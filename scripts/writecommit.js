e9bebfc98dd1da46bafca834fbc461987b8f5956
Gabe616 <poggerschampersqwq@gmail.com>
2023-07-15 02:29:36 +0200

    feat(scripts) update commit msgs

--- scripts/writecommit.js
+++ scripts/writecommit.js
@@ -1,16 +1,26 @@
 import { execSync } from "child_process";
 import { writeFile } from "fs/promises";
 
-const changes = execSync("git status -z -uall").toString().split("\0");
+const changes = execSync("git status -z -uall")
+  .toString()
+  .split("\0")
+  .filter((x) => x.slice(3).startsWith("colors/"));
 
-const folders = changes
-  .filter((x) => {
-    const change = x.slice(0, 2);
-    const file = x.slice(3);
+const folders = changes.map((x) => [
+  x.slice(0, 2).trim(),
+  x.slice(3).split("/")[1],
+]);
 
-    return ["??", "A ", " M"].includes(change) && file.startsWith("colors/");
-  })
-  .map((x) => x.slice(3).split("/")[1])
-  .filter((x, i, a) => !a.slice(0, i).includes(x) && x !== "latest");
+const dchanges = {
+  added: folders.filter((x) => ["A", "??"].includes(x[0])).map((x) => x[1]),
+  updated: folders.filter((x) => x[0] === "M").map((x) => x[1]),
+  deleted: folders.filter((x) => x[0] === "D").map((x) => x[1]),
+};
 
-await writeFile("./commit.txt", `chore: add ${folders.join(", ")}`);
+await writeFile(
+  "./commit.txt",
+  `chore: ${Object.entries(dchanges)
+    .filter((x) => x[1][0])
+    .map((x) => `${x[0]} ${x[1].join(", ")}`)
+    .join(", ")}\n${changes.map((x) => `- ${x}`).join("\n")}`
+);
