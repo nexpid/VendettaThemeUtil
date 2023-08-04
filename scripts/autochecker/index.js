f6c0a4377103865051166db6c3f6ab7ab5f8d49c
Gabe616 <poggerschampersqwq@gmail.com>
2023-08-04 23:43:49 +0200

    feat(autochecker): add version input

--- scripts/autochecker/index.js
+++ scripts/autochecker/index.js
@@ -6,14 +6,23 @@ import { join } from "path";
 
 const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
+const ghBody = (version) =>
+  JSON.stringify({
+    ref: "main",
+    inputs: {
+      version,
+    },
+  });
 const ghArgs = [
   "-X POST",
   '-H "Accept: application/vnd.github+json"',
   '-H "X-GitHub-Api-Version: 2022-11-28"',
   '-F ref="main"',
   "repos/Gabe616/VendettaThemeUtil/actions/workflows/runner.yml/dispatches",
+  "--input -",
 ];
-const runWorkflow = () => execSync(`gh api ${ghArgs.join(" ")}`);
+const runWorkflow = (ver) =>
+  execSync(`echo ${ghBody(ver)} | gh api ${ghArgs.join(" ")}`);
 
 cron.schedule("0,30 * * * * *", async () => {
   const versions = await (await fetch("https://vd.k6.tf/tracker/index")).json();
@@ -22,7 +31,9 @@ cron.schedule("0,30 * * * * *", async () => {
 
   if (last !== "" && current !== last) {
     console.log(`New Discord Alpha! ${last} -> ${current}`);
-    console.log(`API Response: ${runWorkflow().toString() ?? "<empty>"}`);
+    console.log(
+      `API Response: ${runWorkflow(current).toString() ?? "<empty>"}`
+    );
   }
 
   await writeFile(join(__dirname, "lastVersion.txt"), current);
