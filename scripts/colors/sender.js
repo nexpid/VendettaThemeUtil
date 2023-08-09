eb6ff7adf66898f8bf6e1ef37db1961eaa84a6d6
Gabe616 <poggerschampersqwq@gmail.com>
2023-08-09 22:46:10 +0200

    feat(scripts): improve color sender, sends second message with diff

--- scripts/colors/sender.js
+++ scripts/colors/sender.js
@@ -1,6 +1,14 @@
 import { join } from "path";
+import { execSync } from "child_process";
 import getchanges, { only } from "../getchanges.js";
 import { readFile, readdir } from "fs/promises";
+
+process.emit = (
+  (original) =>
+  (...args) =>
+    !(args[0] === "warning" && args[1]?.name === "ExperimentalWarning") &&
+    original.apply(process, args)
+)(process.emit);
 
 const user = {
   name: "GitHub Actions",
@@ -9,6 +17,7 @@ const user = {
 };
 const types = {
   json: "application/json",
+  diff: "plain/text",
 };
 
 const webhook = process.argv.slice(2).join(" ");
@@ -17,25 +26,20 @@ if (!webhook) throw new Error("No webhook specified blehh");
 const thread = "1129770152112697424";
 
 const folders = only(getchanges("colors"))
-  .filter((x) => ["A", "??"].includes(x[0].trim()))
+  .filter((x) => ["A", "??"].includes(x[0].trim()) && x[1] !== "latest")
   .map((x) => x[1]);
 
 let count = 0;
-let lastMessage;
 for (const x of folders) {
   const files = await readdir(join("colors", x));
-  console.log(
-    `Sending ${x} — ${files.join(", ")}${
-      lastMessage ? ` (followup to ${lastMessage})` : ""
-    }`
-  );
+  console.log(`Sending ${x} — ${files.join(", ")}`);
 
   const query = new URLSearchParams();
   query.append("wait", "true");
   if (thread) query.append("thread_id", thread);
 
   const fd = new FormData();
-  for (const i in files) {
+  for (let i = 0; i < files.length; i++) {
     const file = files[i];
     const guh = file.split(".");
 
@@ -70,11 +74,81 @@ for (const x of folders) {
     })
   ).json();
 
-  if (response.id) {
-    lastMessage = response.id;
-    count++;
-  } else
-    console.log(`Failed to send message (${x})! ${JSON.stringify(response)}`);
+  if (!response.id) {
+    console.log(`Failed to send message 1 (${x})! ${JSON.stringify(response)}`);
+    continue;
+  }
+
+  count++;
+
+  const diff = execSync(`git diff --cached colors/${x}`)
+    .toString()
+    .replace(/\r/g, "")
+    .split("\n");
+
+  const changes = {};
+  for (let i = 0; i < diff.length; i++) {
+    const l = diff[i];
+    if (l.startsWith("diff --git")) {
+      const file = l.split("/").reverse()[0];
+      let target = i + 4;
+      for (; target < diff.length; ) {
+        if (diff[target].startsWith("diff --git")) break;
+        target++;
+      }
+
+      changes[file.split(".").slice(0, -1).join(".")] = diff
+        .slice(i + 4, target)
+        .join("\n");
+    }
+  }
+
+  const tooLarge = Object.values(changes).join("\n").length > 100;
+
+  const fdDiff = new FormData();
+  if (!tooLarge)
+    for (let i = 0; i < Object.keys(changes).length; i++) {
+      const [name, content] = Object.entries(changes);
+
+      fdDiff.append(
+        `files[${i}]`,
+        new Blob([content], { type: types.diff }),
+        `${name}-${x}.diff`
+      );
+    }
+
+  fdDiff.append(
+    "payload_json",
+    JSON.stringify({
+      content: Object.keys(changes)[0]
+        ? `Color changes for **${x}**${
+            tooLarge
+              ? `:\n\n${Object.entries(changes)
+                  .map((y) => `\`${y[0]}-${x}.json\` \`\`\`diff\n${y[1]}\`\`\``)
+                  .join("\n")}`
+              : ""
+          }`
+        : `No color changes for **${x}**!`,
+      username: user.name,
+      avatar_url: user.avatar,
+      allowed_mentions: {
+        parse: [],
+      },
+    })
+  );
+  const responseDiff = await (
+    await fetch(`${webhook}?${query.toString()}`, {
+      method: "POST",
+      body: fdDiff,
+    })
+  ).json();
+
+  if (!responseDiff.id) {
+    console.log(`Failed to send message 2 (${x})! ${JSON.stringify(response)}`);
+    continue;
+  }
+
+  count++;
 }
 
 console.log(`Sent messages (${count})`);
