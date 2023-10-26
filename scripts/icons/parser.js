f594db7705d9c3fe816250773dc02ab61a112bcf
Gabe616 <poggerschampersqwq@gmail.com>
2023-10-26 15:51:08 +0200

    fix(scripts): nya nya nya

--- scripts/icons/parser.js
+++ scripts/icons/parser.js
@@ -22,10 +22,16 @@ const recursive = async (dir, path) => {
   }
 };
 
-await recursive(join("tmp", "discordapk", "base"), ["base"]);
-await recursive(join("tmp", "discordapk", "xxhdpi"), ["xxhdpi"]);
+await recursive(join(worker ? "../tmp" : "tmp", "discordapk", "base"), [
+  "base",
+]);
+await recursive(join(worker ? "../tmp" : "tmp", "discordapk", "xxhdpi"), [
+  "xxhdpi",
+]);
 
-const lines = (await readFile("tmp/decompiled.js", "utf8"))
+const lines = (
+  await readFile(`${worker ? "./../" : ""}tmp/decompiled.js`, "utf8")
+)
   .split("\n")
   .map((x) => x.trim());
 
@@ -71,19 +77,14 @@ for (let line = 0; line < lines.length; line++) {
 console.log(`Got ${iconmap.length} icons (${notfound} not found)`);
 console.log("Writing...");
 
-const iconPaths = {
-  ver: join("icons", ver),
-  latest: join("icons", "latest"),
-};
-
-await rm(iconPaths.ver, { recursive: true, force: true });
+await rm(ver, { recursive: true, force: true });
 
 for (const x of iconmap) {
-  const pathVer = join(iconPaths.ver, ...x.path);
+  const pathVer = join(ver, ...x.path);
   await mkdir(pathVer, { recursive: true });
   await copyFile(x.source, join(pathVer, x.icon));
 }
 
 console.log(`Wrote to icons/${ver}`);
-await symlinkDir(pat, iconPaths.latest, { overwrite: true });
+await symlinkDir(ver, "latest", { overwrite: true });
 console.log(`Created symlink icons/${ver} => icons/latest`);
